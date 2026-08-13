1acf3b129b96136c2a6f73762d8914437f60c406
lo-han <lohan.chuan123@gmail.com>
2026-08-13 20:42:36 -0300

    Refactor: Updated calculateVolume class

--- core/tank/calculate_volume.go
+++ core/tank/calculate_volume.go
@@ -4,6 +4,7 @@ import (
 	"archimedes-server/core/log"
 	"context"
 	"errors"
+	"math"
 )
 
 type calculateVolume struct {
@@ -29,13 +30,59 @@ func (c *calculateVolume) CalculateVolume(ctx context.Context, tankID string, fl
 
 	switch tankShape.Shape {
 	case Cylindrical:
-		return c.CylindricalPrototype().Calculate(ctx, tankShape, fluidHeight)
+		return NewCylindricalShape().Calculate(ctx, tankShape, fluidHeight)
 	default:
 		log.Log("error on calculating tank volume: unknown tank shape")
 		return 0, nil
 	}
 }
 
-func (c *calculateVolume) CylindricalPrototype() IShape {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type cylindricalShape struct{}
+
+func NewCylindricalShape() *cylindricalShape {
 	return &cylindricalShape{}
+}
+
+func (c *cylindricalShape) Calculate(ctx context.Context, tank *TankShape, fluidHeight float64) (float64, error) {
+	var tankRadius float64
+	var tankHeight float64
+
+	if radiusKey, ok := tank.Dimensions["radius"]; ok {
+		tank.Dimensions["radius"] = radiusKey
+
+		if r, ok := radiusKey.(float64); ok {
+			tankRadius = r
+		} else {
+			err := errors.New("radius is not a float64, skipping...")
+			log.Log(err.Error())
+			return 0, err
+		}
+
+	} else {
+		err := errors.New("radius not found, skipping...")
+		log.Log(err.Error())
+		return 0, err
+	}
+
+	if heightKey, ok := tank.Dimensions["height"]; ok {
+		tank.Dimensions["height"] = heightKey
+
+		if r, ok := heightKey.(float64); ok {
+			tankHeight = r
+		} else {
+			err := errors.New("height is not a float64, skipping...")
+			log.Log(err.Error())
+			return 0, err
+		}
+
+	} else {
+		err := errors.New("height not found, skipping...")
+		log.Log(err.Error())
+		return 0, err
+	}
+
+	return math.Pi * (tankRadius * tankRadius) * (tankHeight - fluidHeight), nil
+
 }
